466b4af2eaa3d347f53b87fb3d4b94dc2811f9f3
coder9430 <cse21bcsc01@silicon.ac.in>
2024-06-09 22:58:32 +0530

    adding all from

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,10 +1,26 @@
 import React from 'react'
+import Navbar from '../components/Navbar';
+import ApplicantForm from '../components/ApplicantForm';
+import Login from '../components/Login';
+import CompanyForm from '../components/CompanyForm';
+import JobpostForm from '../components/JobpostForm';
 
 const Home = () => {
   return (
     <div>
       <div>
-        Hello Welcome to Our job Portal
+        <Navbar></Navbar>
+        <ApplicantForm></ApplicantForm>
+        <br />
+        <br />
+        <Login></Login>
+        <br />
+        <br />
+        <CompanyForm></CompanyForm>
+        <br />
+        <br />
+        <JobpostForm></JobpostForm>
+
       </div>
     </div>
   )
